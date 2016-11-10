d505073d341ca05acd5b935d9f043861ea2d6099
Arthur Oliveira <arthur.oliveira@locaweb.com.br>
2016-11-10 03:34:26 -0200

    Fix position

--- robot/position.rb
+++ robot/position.rb
@@ -1,9 +1,32 @@
-module Robot
-  class Position
-    attr_accessor :x, :y
-    def initialize(x, y)
-      @x = x
-      @y = y
+class Position
+  attr_reader :x, :y, :direction
+
+  DIRECTIONS = %w(NORTH EAST SOUTH WEST).freeze
+
+  def initialize(x, y, direction)
+    @x = x
+    @y = y
+    @direction = direction
+  end
+
+  def move(direction)
+    case direction
+    when 'NORTH'
+      Position.new(@x, @y + 1, @direction)
+    when 'EAST'
+      Position.new(@x + 1, @y, @direction)
+    when 'SOUTH'
+      Position.new(@x, @y - 1, @direction)
+    when 'WEST'
+      Position.new(@x - 1, @y, @direction)
     end
+  end
+
+  def left
+    DIRECTIONS[(DIRECTIONS.index(direction) - 1)]
+  end
+
+  def right
+    DIRECTIONS[(DIRECTIONS.index(direction) + 1)]
   end
 end
